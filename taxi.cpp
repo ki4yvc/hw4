588774fbcfeda9aaf2b926b68d76b492919abcbe
ki4yvc <dawilco@koala9.cs.clemson.edu>
2016-11-18 11:18:39 -0500

    implmented taxi, truck, and vehicle classes

---
+++ taxi.cpp
@@ -0,0 +1,46 @@
+#include "taxi.h"
+
+Taxi::Taxi();
+{
+
+}
+
+Taxi::Taxi(ifstream &infile);
+{
+
+}
+
+Taxi::Taxi(double fuel, string color);
+{
+
+}
+
+Taxi::Taxi(double);
+{
+
+}
+
+Taxi::~Taxi();
+{
+
+}
+
+virtual bool Taxi::hasPossession() const;
+{
+
+}
+
+void Taxi::setCustomers(bool);
+{
+
+}
+
+static int Taxi::getCount();
+{
+
+}
+
+static void Taxi::incCount();
+{
+
+}
---
+++ truck.cpp
@@ -0,0 +1,46 @@
+#include "truck.h"
+
+Truck::Truck();
+{
+
+}
+
+Truck::Truck(double);
+{
+
+}
+
+Truck::Truck(ifstream &infile);
+{
+
+}
+
+Truck::Truck(double fuel, string color);
+{
+
+}
+
+Truck::~Truck();
+{
+
+}
+
+virtual bool Truck::hasPossession() const;
+{
+
+}
+
+void Truck::setCargo(bool);
+{
+
+}
+
+static int Truck::getCount();
+{
+
+}
+
+static void Truck::incCount();
+{
+
+}
